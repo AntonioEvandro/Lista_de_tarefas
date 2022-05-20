4794cb40b77e1901cc2f9abe02b14bdb149b894b
AntonioEvandro <100598553+AntonioEvandro@users.noreply.github.com>
2022-05-20 11:30:34 -0300

    Primeiro commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,10 @@
-const ola = document.querySelector("#exemplo1")
-
-/*//Errada
-ola.addEventListener("click",alert ("Olá!"))
-*/
-
-/*//certa
-ola.addEventListener("click", function(){
-    alert("Olá!")
-})
-*/
-
-//outra certa
-function cumprimentar (){
-    let nome = prompt("Qual seu nome?")
-    alert("Olá," + nome)
-}
-ola.addEventListener("click", cumprimentar)
-
-
-// 2ª parte da pratica
-
 const btnAddTarefa = document.querySelector("#add_tarefa")
 const lstAFazer = document.querySelector("#a_fazer")
 const lstFeito = document.querySelector("#feito")
 
-const adicionarTarefa = function(){
+const adicionarTarefa = function(ev){
+    ev.preventDefault()
+    
     const tarefa = document.querySelector("#tarefa").value
 
     if(tarefa.length < 1){
@@ -36,24 +16,17 @@ const adicionarTarefa = function(){
 
     lstAFazer.appendChild(novaTarefa)
 }
-btnAddTarefa.addEventListener("click", adicionarTarefa)
 
-/*//  Nao vai funcionar com tds
-let itens = lstAFazer.querySelectorAll("li")
-for(let item of item){
-    item.addEventListener("click", function(){
-        alert("Item clicavel")
-    })
-}*/
-/*
-lstAFazer.addEventListener("click", function(ev){
-    alert("Você clicou em " + ev.target.innerText)
-})
-*/
 const moverParaFeito = function(ev){
     const tarefa = ev.target.innerText
+
     const novaTarefa = document.createElement("li")
     novaTarefa.innerText = tarefa
 
     lstFeito.appendChild(novaTarefa)
+    lstAFazer.removeChild(ev.target)
 }
+
+btnAddTarefa.addEventListener("click", adicionarTarefa)
+
+lstAFazer.addEventListener("click", moverParaFeito)
